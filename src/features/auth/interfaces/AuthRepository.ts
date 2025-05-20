ec1d536caf238eadd3a4c1ae35e05bd862e0a1e7
lloydllim <37279932+lloydllim@users.noreply.github.com>
2025-05-20 22:48:16 +0800

    remove AuthRepository

--- src/features/auth/interfaces/AuthRepository.ts
+++
@@ -1,6 +0,0 @@
-import {User} from '@/core/entities/User'
-
-export interface AuthRepository {
-	login(email: string, password: string): Promise<User | null> // returns user or throws error
-	logout(userId: string): Promise<void>
-}
